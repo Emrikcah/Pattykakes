521fbefa9cdf8051c6e179e0452873838ef27471
Emrikcah <curryandr3@gmail.com>
2021-05-11 15:00:18 -0400

    finished desktop nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#77c8b3',
+        secondary:{
+          100: '#f06898',
+          200: '#f595b6'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
