7dc4565f1a961946bc6e021b43110f0bad367a35
Emrikcah <curryandr3@gmail.com>
2021-05-25 15:51:32 -0400

    added dropdown menu  functionality more markup to order.html

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,16 @@
-//mobile nav
+//top mobile nav
 const  menu = document.getElementById('mobile-menu');
 const burger = document.getElementById('burger');
 console.log(menu);
 
 burger.addEventListener('click',()=>{
    menu.classList.toggle('hidden');
+})
+
+//order.html mobile nav
+const chevron = document.getElementById('chevron-down');
+const dropdown = document.getElementById('dropdown-nav');
+
+chevron.addEventListener('click',()=>{
+   dropdown.classList.toggle('hidden');
 })
